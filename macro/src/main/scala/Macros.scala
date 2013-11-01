b29fe3a013366e69b65b864470630c43b28481b8
Jonathan Chow <me@echo.sh>
2013-11-02 01:20:11 +1300

    Adds complete example.

--- macro/src/main/scala/Macros.scala
+++ macro/src/main/scala/Macros.scala
@@ -1,4 +1,36 @@
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
-// write macros here
+trait Mappable[T] {
+  def toMap(t: T): Map[String, Any]
+  def fromMap(map: Map[String, Any]): T
+}
+
+object Mappable {
+  implicit def materializeMappable[T]: Mappable[T] = macro materializeMappableImpl[T]
+
+  def materializeMappableImpl[T: c.WeakTypeTag](c: Context): c.Expr[Mappable[T]] = {
+    import c.universe._
+    val tpe = weakTypeOf[T]
+    val companion = tpe.typeSymbol.asClass.companionSymbol
+
+    val fields = tpe.declarations.collectFirst {
+      case m: MethodSymbol if m.isPrimaryConstructor ⇒ m
+    }.get.paramss.head
+
+    val (toMapParams, fromMapParams) = fields.map { field ⇒
+      val name = field.name
+      val decoded = name.decoded
+      val returnType = tpe.declaration(name).asMethod.returnType
+
+      (q"$decoded → t.$name", q"map($decoded).asInstanceOf[$returnType]")
+    }.unzip
+
+    c.Expr[Mappable[T]] { q"""
+      new Mappable[$tpe] {
+        def toMap(t: $tpe): Map[String, Any] = Map(..$toMapParams)
+        def fromMap(map: Map[String, Any]): $tpe = ${companion}(..$fromMapParams)
+      }
+    """ }
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
 object Main extends App {
-  import Macros._
+  import Mappable._
+  case class Person(name: String, age: Int)
 
-  println("it works!")
+  def mapify[T: Mappable](t: T) = implicitly[Mappable[T]].toMap(t)
+  def materialize[T: Mappable](map: Map[String, Any]) = implicitly[Mappable[T]].fromMap(map)
+
+  val person = Person("john", 24)
+  println(mapify(person))
+  // Map("name" -> "john", "age" -> 24)
+
+  val map = Map("name" -> "bob", "age" -> 22)
+  println(materialize[Person](map))
+  // Person("bob", 22)
 }
