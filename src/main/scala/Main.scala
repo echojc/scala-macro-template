c13fba3493b73e272e45bd580cb25d5adae9fa59
Jonathan Chow <me@echo.sh>
2014-10-18 11:29:59 +1300

    Use assertions instead of printing the results.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,10 +6,12 @@ object Main extends App {
   def materialize[T: Mappable](map: Map[String, Any]) = implicitly[Mappable[T]].fromMap(map)
 
   val person = Person("john", 24)
-  println(mapify(person))
-  // Map("name" -> "john", "age" -> 24)
+  assert {
+    mapify(person) == Map("name" -> "john", "age" -> 24)
+  }
 
   val map = Map("name" -> "bob", "age" -> 22)
-  println(materialize[Person](map))
-  // Person("bob", 22)
+  assert {
+    materialize[Person](map) == Person("bob", 22)
+  }
 }
